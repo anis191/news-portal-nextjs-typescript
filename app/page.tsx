f91128ebbee7aed4dacaea1593fe0e3f112cf963
anisul alam <anisulalam2002@gmail.com>
2026-02-26 16:17:07 +0600

    Fetching data from api server

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 // import Image from "next/image";
+import NewsCard from "@/components/news/NewsCard";
+import Banner from "@/components/shared/Banner";
 
-export default function Home() {
+const Home = async() => {
+  // fetch the data from api endpoint:
+  const data = await fetch('https://news-portal-fastapi-server.vercel.app/news')
+  const news = await data.json();
+  // console.log(news);
+
   return (
-    <div></div>
+    <div className="py-12">
+        <Banner />
+        {/* latest news */}
+        <div className="my-12">
+          <h2 className="text-2xl lg:text-3xl font-bold mb-8">Latest News</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            <NewsCard />
+          </div>
+        </div>
+    </div>
   );
 }
+
+export default Home
